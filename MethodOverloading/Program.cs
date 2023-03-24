b47a4774e69045f3aeeb9a247bfe7510b9df1e0e
Matt Fuller <mnfuller552@gmail.com>
2023-03-23 20:45:28 -0400

    finished method overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,41 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+
+        static int Add(int num1, int num2)
         {
-            Console.WriteLine("Hello, World!");
+            return num1 + num2;
+
+        }
+        static double Add(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+       static string Add(int num1, int num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+
+            return sum.ToString();
+
+            static void Main(string[] args)
+            {
+
+
+
+            }
         }
     }
 }
